a6dce47a78d18cfaae5504e16f1185563e984ecc
FCF5646448 <2998000457@qq.com>
2020-03-22 10:40:01 +0800

    开始学习Flutter组件开发

---
+++ Demos/helloworld/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Wellcome to Flutter'),
+        ),
+        body:Center(
+          child: Text('Hello world'),
+        ),
+      ),
+    );
+  }
+}
