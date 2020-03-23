20fca172f5de07230e2858fd6853199e076aa9c0
FCF5646448 <2998000457@qq.com>
2020-03-23 20:36:47 +0800

    Flutter 开发布局

---
+++ Demos/mostsimpleflutter/lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text('hello flutter1', textDirection:TextDirection.ltr)); //
+  }
+}
